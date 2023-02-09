c6f32d4fb076baba30fef078ed5eba112c65061d
kanikakalra86 <84987241+kanikakalra86@users.noreply.github.com>
2023-02-09 17:04:08 +0100

    Initial files

---
+++ my-money/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter,Switch,Route } from "react-router-dom";
+
+//Pages and components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"; 
+import Signup from "./pages/signup/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = "/">
+            <Home/>
+          </Route>
+          <Route path = "/login">
+            <Login/>
+          </Route>
+          <Route path = "/signup">
+            <Signup/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App
---
+++ my-money/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import styles from './Home.module.css'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        
+        Home
+    
+    </div>
+  )
+}
+
+
