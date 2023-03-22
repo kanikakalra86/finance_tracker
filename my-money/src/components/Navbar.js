27fc530c4fabdfcdfb23c551cf7601a5df4065dc
kanikakalra86 <84987241+kanikakalra86@users.noreply.github.com>
2023-03-22 13:41:42 +0100

    added transaction forms and transaction list for a user and modified home pages accordingly Added firestore rules for authorized user

--- my-money/src/components/Navbar.js
+++ my-money/src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}>Finance Tracker</li>
         {!user && (
         <>
         <li><Link to="/login">Login</Link></li>
